3fccb304dd31af569cd9afa4511bfdf599ce280d
M4TH3US17 <matheusdalvino50@gmail.com>
2021-09-12 16:04:53 -0400

    Creating the service layer

---
+++ src/main/java/com/example/demo/services/ClientService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Client;
+import com.example.demo.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll(){
+		return repository.findAll();
+	}
+	
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
+	}
+}
