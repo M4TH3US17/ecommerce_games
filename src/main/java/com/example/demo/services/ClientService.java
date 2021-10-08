df9f3b2a7e0ad39213d577059399f0fa5ed59c04
M4TH3US17 <matheusdalvino50@gmail.com>
2021-10-08 18:22:04 -0400

    Exception handling - findById

--- src/main/java/com/example/demo/services/ClientService.java
+++ src/main/java/com/example/demo/services/ClientService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Client;
 import com.example.demo.repositories.ClientRepository;
+import com.example.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,7 +22,7 @@ public class ClientService {
 	
 	public Client findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public Client insert(Client obj) {
